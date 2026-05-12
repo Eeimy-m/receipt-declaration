58a26837feeb7a8cb85a27d1656cf8314d3c9635
Elisa <eeimymiura@gmail.com>
2026-05-12 08:35:01 -0300

    GastoSaude constructor, getters and setters

--- src/GastoSaude.java
+++ src/GastoSaude.java
@@ -1,4 +1,24 @@
 public final class GastoSaude extends Gasto {
+    public static final double DEDUCAO_MAX_SAUDE = 1_500.0;
     private String registroConselho;
-    public double DEDUCAO_MAX_SAUDE;
+
+    public GastoSaude(long id, String descricao, double valor, String cnpj, String registroConselho) {
+        super(id, descricao, valor, cnpj);
+        this.registroConselho = registroConselho;
+    }
+
+    @Override
+    public String toString() {
+        return "GastoSaude{" +
+                "registroConselho='" + registroConselho + '\'' +
+                '}';
+    }
+
+    public String getRegistroConselho() {
+        return registroConselho;
+    }
+
+    public void setRegistroConselho(String registroConselho) {
+        this.registroConselho = registroConselho;
+    }
 }
